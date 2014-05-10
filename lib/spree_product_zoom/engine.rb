1fb4f25022b65857f4391a851bbaec9372a59618
Michael Caviness <mcaviness@tcwequipment.com>
2014-05-09 17:16:56 -0700

    Add assets paths to engine initializers so products precompile in production correctly

--- lib/spree_product_zoom/engine.rb
+++ lib/spree_product_zoom/engine.rb
@@ -16,15 +16,19 @@ module SpreeProductZoom
     initializer("spree.product_zoom.preferences", 
                 :after => "spree.environment",
                 :before => :load_config_initializers) do |app|
-      Spree::ProductZoom::Config = Spree::ProductZoomConfiguration.new
-    end
+                  Spree::ProductZoom::Config = Spree::ProductZoomConfiguration.new
+                end
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
+                initializer :assets do |config|
+                  Rails.application.config.assets.precompile += %w( store/fancybox_*.* store/blank.gif )
+                end
 
-    config.to_prepare &method(:activate).to_proc
+                def self.activate
+                  Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+                    Rails.configuration.cache_classes ? require(c) : load(c)
+                  end
+                end
+
+                config.to_prepare &method(:activate).to_proc
   end
 end
